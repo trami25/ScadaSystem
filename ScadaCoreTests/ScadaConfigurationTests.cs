5b8e26d94cbe39579b17b46f57fd1ac9482be80a
Vladislav Radovic <vradovic522@gmail.com>
2024-06-15 18:26:44 +0200

    Add tests for scada config

---
+++ ScadaCoreTests/ScadaConfigurationTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ScadaCore.Configuration;
+using ScadaCore.Tags.Model;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ScadaCoreTests
+{
+    [TestClass]
+    public class ScadaConfigurationTests
+    {
+        public static ScadaConfiguration ScadaConfiguration { get; private set; }
+        public static List<Tag> Tags { get; private set; }
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            ScadaConfiguration = new ScadaConfiguration(@"../../scadaTestConfig.xml");
+            Tags = ScadaConfiguration.getTags().ToList();
+        }
+
+        [TestMethod]
+        public void ShouldReturnCorrectSizeOfList()
+        {
+            Assert.AreEqual(4, Tags.Count);
+        }
+
+        [TestMethod]
+        public void TagShouldHaveCorrectAttributes()
+        {
+            var tag = (AnalogOutputTag)Tags.Where(t => t.Id == "ao1").First();
+
+            Assert.IsNotNull(tag);
+            Assert.AreEqual("deabc", tag.IOAddress);
+            Assert.AreEqual(15.4, tag.InitialValue);
+            Assert.AreEqual(Unit.Kg, tag.Unit);
+        }
+    }
+}

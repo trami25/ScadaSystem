f1e3554823b3313f787ff9532a47467d83669c74
Marija Živanović <zivanovicmarija895@gmail.com>
2024-06-15 16:32:26 +0200

    rtu and rt driver added

---
+++ RTDriver/RTDriver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RTDriver
+{
+    public class RTDriver
+    {
+        private Dictionary<string, List<double>> _data = new Dictionary<string, List<double>>();
+
+        public void ReceiveData(string address, double value)
+        {
+            if (!_data.ContainsKey(address))
+            {
+                _data[address] = new List<double>();
+            }
+            _data[address].Add(value);
+            Console.WriteLine($"Received value {value} at address {address}");
+        }
+
+        public List<double> GetData(string address)
+        {
+            return _data.ContainsKey(address) ? _data[address] : new List<double>();
+        }
+    }
+}
+
---
+++ RTUnit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using RTDriver;
+
+namespace RTUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // RTU configuration
+            string address = "127.0.0.1"; // address of RT Driver
+            string rtuId = "RTU1";
+            double lowerLimit = 0;
+            double upperLimit = 100;
+
+            // Create an instance of RT Driver
+            RTDriver.RTDriver driver = new RTDriver.RTDriver();
+
+            // Simulation of sending data
+            Random random = new Random();
+            while (true)
+            {
+                double value = random.NextDouble() * (upperLimit - lowerLimit) + lowerLimit;
+                driver.ReceiveData(address, value);
+
+                // Wait for 1 second before sending the next value
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
+

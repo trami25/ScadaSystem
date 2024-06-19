94f8b8b5013d5d3b4da52b18589f65a994fdb508
Marija Živanović <zivanovicmarija895@gmail.com>
2024-06-19 10:10:18 +0200

    final fixes

--- RTUnit/Program.cs
+++ RTUnit/Program.cs
@@ -8,7 +8,6 @@ namespace RTUnit
     {
         static void Main(string[] args)
         {
-            // Unos konfiguracionih vrednosti
             Console.WriteLine("Enter RTU ID:");
             string rtuId = Console.ReadLine();
 
@@ -21,19 +20,16 @@ namespace RTUnit
             Console.WriteLine("Enter upper limit:");
             double upperLimit = double.Parse(Console.ReadLine());
 
-            // Kreiramo instancu RT Drivera
             RTDriver.RTDriver driver = new RTDriver.RTDriver();
             driver.ReceiveData(address, lowerLimit, upperLimit);
 
-            // Simulacija slanja podataka
-            Random random = new Random();
+            //Random random = new Random();
             while (true)
             {
                 double value = driver.ReturnValue(address);
                 Console.WriteLine($"Generated value: {value} for address: {address}");
 
-                // Čekamo 10 sekundi pre nego što pošaljemo sledeću vrednost
-                Thread.Sleep(10000);
+                Thread.Sleep(500);
             }
         }
     }

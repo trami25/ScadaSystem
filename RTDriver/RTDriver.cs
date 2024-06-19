0d70a7fc8ec2967473ed969a94cbd916b2fc8446
Marija Živanović <zivanovicmarija895@gmail.com>
2024-06-19 09:28:55 +0200

    rtu sending random values to specific address to rt driver

---
+++ RTDriver/RTDriver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DriverApi;
+
+namespace RTDriver
+{
+    public class RTDriver : IDriver
+    {
+        private Dictionary<string, (double lowerLimit, double upperLimit)> _limits = new Dictionary<string, (double lowerLimit, double upperLimit)>();
+        private Dictionary<string, List<double>> _data = new Dictionary<string, List<double>>();
+        private Random _random = new Random();
+
+        public void ReceiveData(string address, double lowerLimit, double upperLimit)
+        {
+            _limits[address] = (lowerLimit, upperLimit);
+            _data[address] = new List<double>();
+        }
+
+        public double ReturnValue(string address)
+        {
+            if (_limits.ContainsKey(address))
+            {
+                double value = GenerateRandomValue(_limits[address].lowerLimit, _limits[address].upperLimit);
+                _data[address].Add(value);
+                return value;
+            }
+            else
+            {
+                throw new ArgumentException($"Address {address} not found.");
+            }
+        }
+
+        private double GenerateRandomValue(double lowerLimit, double upperLimit)
+        {
+            return _random.NextDouble() * (upperLimit - lowerLimit) + lowerLimit;
+        }
+
+        public List<double> GetDataForAddress(string address)
+        {
+            return _data.ContainsKey(address) ? _data[address] : new List<double>();
+        }
+    }
+}
---
+++ RTUnit/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+using RTDriver;
+
+namespace RTUnit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Unos konfiguracionih vrednosti
+            Console.WriteLine("Enter RTU ID:");
+            string rtuId = Console.ReadLine();
+
+            Console.WriteLine("Enter RT Driver address:");
+            string address = Console.ReadLine();
+
+            Console.WriteLine("Enter lower limit:");
+            double lowerLimit = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter upper limit:");
+            double upperLimit = double.Parse(Console.ReadLine());
+
+            // Kreiramo instancu RT Drivera
+            RTDriver.RTDriver driver = new RTDriver.RTDriver();
+            driver.ReceiveData(address, lowerLimit, upperLimit);
+
+            // Simulacija slanja podataka
+            Random random = new Random();
+            while (true)
+            {
+                double value = driver.ReturnValue(address);
+                Console.WriteLine($"Generated value: {value} for address: {address}");
+
+                // Čekamo 10 sekundi pre nego što pošaljemo sledeću vrednost
+                Thread.Sleep(10000);
+            }
+        }
+    }
+}
